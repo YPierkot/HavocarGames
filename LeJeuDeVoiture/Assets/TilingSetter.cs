25be80d14991e40e6630c13b4d1ed7c99d8b43a5
Mathis Jibert <mathis.jibert@gmail.com>
2023-12-19 18:28:45 +0100

    Tiling Part 1

--- LeJeuDeVoiture/Assets/TilingSetter.cs
+++ LeJeuDeVoiture/Assets/TilingSetter.cs
@@ -24,15 +24,25 @@ public class TilingSetter : MonoBehaviour
     [ContextMenu("SetTiling")]
     public void SetTiling()
     {
-        /*int tileXAmount = Mathf.RoundToInt(rectTransform.rect.width / tilingSize.x);
+        int tileXAmount = Mathf.RoundToInt(rectTransform.rect.width / tilingSize.x);
         int tileYAmount = Mathf.RoundToInt(rectTransform.rect.height / tilingSize.y);
 
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            DestroyImmediate(tiles[i]);
+        }
+        tiles.Clear();
+        
         for (int x = 0; x < tileXAmount; x++)
         {
             for (int y = 0; y < tileYAmount; y++)
             {
-                Vector3 pos = new Vector3(-rectTransform.rect.width / 2 + (x * tilingSize.x),0,);
+                Vector3 pos = new Vector3(-rectTransform.rect.width / 2f + (tilingSize.x / 2) + (x * tilingSize.x),-rectTransform.rect.height / 2f + (tilingSize.y / 2) + (y * tilingSize.y),0);
+                tiles.Add(Instantiate(possibleTiles[Random.Range(0, possibleTiles.Length)], Vector3.zero, Quaternion.identity, transform));
+                tiles[tiles.Count - 1].transform.localPosition = pos;
             }
-        }*/
+        }
+        
+        SetRandomAngles();
     }
 }
